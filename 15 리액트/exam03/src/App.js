5ec13fa3b893ae1951a123c9c528284cbb06121a
rmftp <kyj2001kyj@naver.com>
2025-06-13 17:38:14 +0900

    toto S

--- 15 리액트/exam03/src/App.js
+++ 15 리액트/exam03/src/App.js
@@ -2,16 +2,17 @@ import MyComponent from './components/MyComponent';
 import MyComponent2 from './components/MyComponent2';
 import Counter from './components/Counter';
 
-function App() {
-  return (
-    <>
-      <h1>안녕하세요</h1>
-      <h2>반갑습니다</h2>
-      <MyComponent color="purple" favoriteNumber={7} />
-      <MyComponent2 color="red" background="skyblue" />
-      <Counter />
-    </>
-  );
-}
+// function App() {
+// return (
+//   <>
+//     <h1>안녕하세요</h1>
+//     <h2>반갑습니다</h2>
+//     <MyComponent color="purple" favoriteNumber={7} />
+//     <MyComponent2 color="red" background="skyblue" />
+//     <Counter />
+//   </>
+// );
+// }
+import Counter2 from './components/Counter2';
 
 export default App;
---
+++ 15 리액트/exam03/src/components/Counter2.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+const Counter2 extends Component {
+  state = {
+    number:0,
+  };
+  render() {
+    const { number } = this.state;
+    return(
+      <>
+        <h1>{number}</h1>
+        <button type='button' onClick={() => setNumber((prevNumber) => prevNumber + 1)}>-1</button>
+        <button type='button' onClick={() => this.setState({number: number + 1})}>+1</button>
+      </>
+    )
+  }
+};
+
+export default Counter;
